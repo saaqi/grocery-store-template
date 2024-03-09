1378699a98ba40653500905ba0e2007d00d41209
Saqib Islam <saaqi.grw@gmail.com>
2024-03-09 13:09:01 +0500

    MINOR

--- src/js/main.js
+++ src/js/main.js
@@ -7,22 +7,12 @@ import "boxicons/css/boxicons.min.css";
 
 
 // --- Import only the required components.
-// import { Alert } from 'bootstrap';
-// import { Button } from 'bootstrap';
-// import { Carousel } from "bootstrap";
-// import { Collapse } from "bootstrap";
-// import { Dropdown } from "bootstrap";
-import { Modal } from "bootstrap";
-import { Offcanvas } from "bootstrap";
-// import { Popover } from "bootstrap";
-import { ScrollSpy } from "bootstrap";
-// import { Tab } from "bootstrap";
-// import { Toast } from "bootstrap";
-// import { Tooltip } from "bootstrap";
-
+import "bootstrap/js/dist/modal.js";
+import "bootstrap/js/dist/offcanvas.js";
+import "bootstrap/js/dist/scrollspy.js";
 
 // Setup Products Data
-import "./products.js";
+import "./product-cards.js";
 
 // Setup Glide.js
 import "./glide.js";
